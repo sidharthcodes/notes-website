22ba9c545d2fd67dcb210fc91ef6163027189b53
Sidharth <sidnagar08@gmail.com>
2021-04-29 09:44:14 +0530

    express server

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+
+// const dishRouter = require('./routes/dishRouter');
+// const promoRouter = require('./routes/promoRouter');
+// const leaderRouter = require('./routes/leaderRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev')); // shows information about headers;
+
+
+// app.use('/dishes',dishRouter);
+// app.use('/dishes/:dishId',dishRouter);
+// app.use('/promotions',promoRouter);
+// app.use('/promotions/promoId',promoRouter);
+// app.use('/leaders',leaderRouter);
+// app.use('/leaders/leaderId',leaderRouter);
+
+// app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname)); // search the given directory for serving the static pages;
+
+app.use((req,res,next) => {		// will use in creating a server;
+		res.statusCode = 200;
+		res.setHeader('Content-Type','text/html');
+		res.end('<html><body><h1>This is a Express server</h1></body></html>');
+});
+
+const server = http.createServer(app); 	// creates a server;
+
+server.listen(port,hostname,()=>{		//listening to the requests at the localhost:port;
+	console.log(`Listening to http://${hostname}:${port}`);
+});
